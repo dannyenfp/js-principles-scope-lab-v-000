6c4f1a738d56207afc8f485fb635100d98e5aae9
Daniel Valmas <daniel.valmas@gmail.com>
2018-08-21 00:04:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ function upperCaseCustomerName() {
 }
 
 function setBestCustomer() { 
-  var bestCustomer = 'not bob';
-  return bestCustomer; 
+  return var bestCustomer = 'not bob';
 } 
 
 function overwriteBestCustomer() { 
